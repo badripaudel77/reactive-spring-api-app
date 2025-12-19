1e5f23c8db6d93d5eda4149b0e818f945e7ddddd
Badri Paudel <thecrazymanav@gmail.com>
2025-12-19 13:39:02 -0600

    Stream Value Emition endpoint added

--- src/main/java/com/fairfree/reactive/controller/ItemController.java
+++ src/main/java/com/fairfree/reactive/controller/ItemController.java
@@ -4,14 +4,17 @@ import com.fairfree.reactive.model.Item;
 import com.fairfree.reactive.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @RestController
 @RequestMapping("/api/items")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ItemController {
 
     private final ItemService itemService;
@@ -31,5 +34,14 @@ public class ItemController {
     public Flux<Item> getItemsByOwner(@PathVariable Long ownerId) {
         return itemService.getItemsByOwner(ownerId);
     }
-}
 
+    @GetMapping(value = "/test", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Item> testItem() {
+        return Flux.just(
+                new Item(1L, "Jacket", "Winter One", 2, 5L),
+                new Item(2L, "Coat", "Winter Two", 3, 5L),
+                new Item(3L, "Hat", "Summer One", 1, 5L)
+        ).delayElements(Duration.ofSeconds(2));
+        // .collectList(); Use it for collecting data at once. Still reactive but sends data to client at once.
+    }
+}
